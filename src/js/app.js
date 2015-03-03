51ee4fccd30003816d74bf9efb929ddc8d888411
Lewis Bayfield <bassymew@gmail.com>
2015-03-03 18:07:32 +0000

    angular routing

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 if(typeof angular != 'undefined') {
-    var app = angular.module('mcPortfolioApp', [])
+    var app = angular.module('mcPortfolioApp', [
+        'ui.router'
+    ])
+
+    app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url : '/',
+                templateUrl : 'templates/home.html',
+                controller : 'homeCtrl'
+            })
+    }])
 }
